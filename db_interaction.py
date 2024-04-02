725f33966a18767c09e16c58ecf378c7b4edd05a
Liam <2895711m@student.gla.ac.uk>
2024-04-03 00:23:29 +0100

    Integrate SQLITE3 #12

---
+++ db_interaction.py
@@ -0,0 +1,55 @@
+import sqlite3
+
+class DatabaseManager:
+    def __init__(self, db_name='database.db'):
+        # Connect to the database (creates a new database if it doesn't exist)
+        self.conn = sqlite3.connect(db_name)
+
+        # Create a cursor object to execute SQL queries
+        self.cursor = self.conn.cursor()
+
+        # Create a table for users
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+				poke_bits NUMBER DEFAULT 1500 NOT NULL
+            )
+        ''')
+
+        # Create a table for Pokemon
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS pokemon (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                price REAL NOT NULL,
+                user_id INTEGER,
+                FOREIGN KEY(user_id) REFERENCES users(id)
+            )
+        ''')
+
+        # Commit the changes
+        self.conn.commit()
+
+    def insert_pokemon(self, pokemon_list, user_id):
+        for pokemon in pokemon_list:
+            self.cursor.execute("INSERT INTO pokemon (name, price, user_id) VALUES (?, ?, ?)", (pokemon['name'], pokemon['price'], user_id))
+        self.conn.commit()
+
+    def insert_user(self, username, password):
+    	self.cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+    	self.conn.commit()
+
+    def close_connection(self):
+        self.conn.close()
+
+    def get_users(self):
+    	self.cursor.execute("SELECT * FROM users")
+    	users = self.cursor.fetchall()
+    	return users
+
+    def get_pokemon(self, user_id):
+    	self.cursor.execute("SELECT * FROM pokemon WHERE user_id = ?", (user_id,))
+    	pokemon = self.cursor.fetchall()
+    	return pokemon
---
+++ tests/db_interaction_test.py
@@ -0,0 +1,73 @@
+import unittest
+import sqlite3
+import os
+from db_interaction import DatabaseManager
+
+class TestDatabaseManager(unittest.TestCase):
+    def setUp(self):
+        # Create a new database for testing
+        self.db = DatabaseManager('test.db')
+
+    def tearDown(self):
+        # Close the connection and remove the test database after each test
+        self.db.conn.close()
+        os.remove('test.db')
+
+    def test_insert_user(self):
+        # Define a test user
+        username = 'test_user'
+        password = 'test_password'
+
+        # Insert the user into the database
+        self.db.insert_user(username, password)
+
+        # Query the database to check if the user was inserted correctly
+        self.db.cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
+        data = self.db.cursor.fetchone()
+
+        # Check that the data matches the inserted user
+        self.assertEqual(data[1], username)
+        self.assertEqual(data[2], password)
+
+    def test_get_users(self):
+        # Define a test user
+        username = 'test_user'
+        password = 'test_password'
+
+        # Insert the user into the database
+        self.db.insert_user(username, password)
+
+        # Get the users from the database
+        users = self.db.get_users()
+
+        # Check that the returned data matches the inserted user
+        self.assertEqual(users[0][1], username)
+        self.assertEqual(users[0][2], password)
+        self.assertEqual(users[0][3], 1500)
+
+    def test_get_pokemon(self):
+        # Define a test user and Pokemon
+        username = 'test_user'
+        password = 'test_password'
+        pokemon_list = [{'name': 'Pikachu', 'price': 100.0}]
+
+        # Insert the user into the database
+        self.db.insert_user(username, password)
+
+        # Get the user_id of the inserted user
+        self.db.cursor.execute('SELECT id FROM users WHERE username = ?', (username,))
+        user_id = self.db.cursor.fetchone()[0]
+
+        # Insert the Pokemon into the database
+        self.db.insert_pokemon(pokemon_list, user_id)
+
+        # Get the Pokemon from the database
+        pokemon = self.db.get_pokemon(user_id)
+
+        # Check that the returned data matches the inserted Pokemon
+        self.assertEqual(pokemon[0][1], 'Pikachu')
+        self.assertEqual(pokemon[0][2], 100.0)
+        self.assertEqual(pokemon[0][3], user_id)
+
+if __name__ == '__main__':
+    unittest.main()

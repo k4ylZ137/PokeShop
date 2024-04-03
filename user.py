a18eeef8ef9ec592159c056d2dd3c93abe6cb021
Liam <2895711m@student.gla.ac.uk>
2024-04-03 12:56:17 +0100

    Create user class #13

---
+++ user.py
@@ -0,0 +1,32 @@
+import bcrypt
+
+class user:
+    _all_users = []
+
+    def __init__(self, username, password, inventory=[]):
+        self.username = username
+        self.inventory = inventory
+        self.pokebits = 1500
+
+        self._all_users.append(self)
+
+    def get_username(self):
+        return self.username
+
+    def get_inventory(self):
+        return self.inventory
+
+    def get_pokebits(self):
+        return self.pokebits
+
+    def update_pokebits(self, amount):
+        self.pokebits = self.pokebits + amount
+        if self.pokebits < -1500:
+                print("Insufficient funds for this operation. We are seizing your pokemon and deactiving your account.")
+        return self.pokebits
+
+    def set_current_user(self, user):
+         self.current_user = user
+
+    def get_current_user(self):
+         return self.current_user

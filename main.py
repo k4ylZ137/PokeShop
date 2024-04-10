ab9d5826d5b6ae09d0e74302d5cb118d91f7b631
Liam <2895711m@student.gla.ac.uk>
2024-04-10 14:15:37 +0100

    Inital sign in/ sign up logic #4

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-# New file
+from inputs import *
+from user import *
+from account_verification import *
+
+def main():
+    print("\n" + "-"*60)
+    print("\nWould you like to Sign-up or Sign-in? (sign-up/sign-in)\n")
+    print("-"*60)
+
+    #Gets the user to pick if they want to sign in or sign up
+    input_dict = ({
+            'sign-up': ('Allows you to create a new account', sign_up),
+            'sign-in': ('Allows you to log into your account', sign_in),
+        })
+
+    handle_inputs(input_dict)
+
+main()

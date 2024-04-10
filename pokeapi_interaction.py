be72a37c0459ed68d8d0e3146d6e9824b7d1732c
Arran22 <2896440D@student.gla.ac.uk>
2024-04-10 14:45:04 +0100

    Basic poke_api integration working

---
+++ pokeapi_interaction.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+def get_poke_data():
+    url = f"https://pokeapi.co/api/v2/pokemon/"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        pokemen = response.json()
+        return (pokemen)
+    else:
+        print(f'Error: {response.status_code}')
+    # pokemen = json.loads(str(response))
+    # print(pokemen)
+
+pokemen = get_poke_data()
+
+

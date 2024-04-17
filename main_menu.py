c126c40ef97245bab93191a3c69b7f8ad0947434
Liam <2895711m@student.gla.ac.uk>
2024-04-17 12:45:39 +0100

    feat(main-menu): add main menu page #33

--- main_menu.py
+++ main_menu.py
@@ -1,26 +1,43 @@
 import inputs
-import rich
+from rich import print
 from rich.panel import Panel
 
-def render_main_menu():
-	"""
-	Render the main menu.
-	"""
-	print()
-	title = 'Welcome to the Pokemon Market!'
-	text = '\tWould you like to Search, Shop or look at your Inventory? (search/shop/inventory)'
-	rich.print(Panel(text, title=title, padding=(1,1)))
+from interaction.db_interaction import database_manager
 
-	# Define input dictionary
-	input_dict = {
-		'shop': ('Search the platform for listed pokemon to buy', shop_func),
-		'inventory': ('Look at your inventory and list pokemon', inventory_func)
-	}
+from store import store
+from inventory import inventory
 
-	inputs.handle_inputs(input_dict)
+class menu:
+    def __init__(self, current_user):
+        self.USER = current_user
 
-def shop_func():
-    print('shopping...')
+    def render_main_menu(self):
+        while True:
+            """
+            Render the main menu.
+            """
+            print()
+            title = 'Welcome to the Pokemon Market! 🏪'
+            text = '\tWould you like to look at the Store or your Inventory? (shop/inventory)'
+            print(Panel(text, title=title, padding=(1,1)))
 
-def inventory_func():
-    print('inventory...')
+            # Define input dictionary
+            input_dict = {
+                'shop': ('Search the platform for listed pokemon to buy', self.shop_func),
+                'inventory': ('Look at your inventory and list pokemon', self.inventory_func)
+            }
+
+            inputs.handle_inputs(input_dict)
+
+    def shop_func(self):
+        db = database_manager()
+        listed_pokemon = db.get_pokemon_listed()
+
+        shop_page = store(self.USER, listed_pokemon)
+
+    def inventory_func(self):
+        db = database_manager()
+        listed_pokemon = db.get_pokemon(self.USER)
+
+        inv_page = inventory(self.USER, listed_pokemon)
+
